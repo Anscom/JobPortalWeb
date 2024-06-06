708b0963f8a0198e4a929c1eaabd96e4e9339b9f
Anscom Ooi <anscom.ooi@neurogine.com>
2024-06-06 11:56:52 +0800

    job candidate profile

---
+++ jobportal/src/main/java/com/anscom/jobportal/services/JobSeekerProfileService.java
@@ -0,0 +1,24 @@
+package com.anscom.jobportal.services;
+
+import com.anscom.jobportal.entity.JobSeekerProfile;
+import com.anscom.jobportal.repository.JobSeekerProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class JobSeekerProfileService {
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+    }
+
+    public Optional<JobSeekerProfile> getOne(Integer id) {
+        return jobSeekerProfileRepository.findById(id);
+    }
+
+    public JobSeekerProfile addNew(JobSeekerProfile jobSeekerProfile) {
+        return jobSeekerProfileRepository.save(jobSeekerProfile);
+    }
+}
