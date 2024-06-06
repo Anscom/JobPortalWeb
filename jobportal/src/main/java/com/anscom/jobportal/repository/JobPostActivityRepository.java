d24d712c3f5c405749c441615697e115dde02cad
Anscom Ooi <anscom.ooi@neurogine.com>
2024-06-06 10:11:50 +0800

    recruiter post new

---
+++ jobportal/src/main/java/com/anscom/jobportal/repository/JobPostActivityRepository.java
@@ -0,0 +1,7 @@
+package com.anscom.jobportal.repository;
+
+import com.anscom.jobportal.entity.JobPostActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+}
